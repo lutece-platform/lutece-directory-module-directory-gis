b00fed33379a79fd5c1608bf23cdb5f320ded6d2
thomas dumont <thomas.dumont@paris.fr>
2017-07-24 14:12:46 +0200

    MDIRECTGIS-7 : Implement getParameter(int) in GisProvider.java

--- src/java/fr/paris/lutece/plugins/directory/modules/gis/service/GisProvider.java
+++ src/java/fr/paris/lutece/plugins/directory/modules/gis/service/GisProvider.java
@@ -131,4 +131,12 @@ public class GisProvider implements IMapProvider
 	{
 		return CONSTANT_MAP_LIST_SUPPORTED;
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Object getParameter( int nKey )
+	{
+		return null;
+	}
 }
